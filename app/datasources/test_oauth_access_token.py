f45aab32f0c0c0c35c440f35a568a3b068b1f90e
Mihai Parparita <mihaip@chromium.org>
2011-01-26 21:43:03 -0800

    Test all access tokens.

--- app/datasources/test_oauth_access_token.py
+++ app/datasources/test_oauth_access_token.py
@@ -7,8 +7,9 @@ if len(sys.argv) != 2:
     exit(1)
 
 service_provider = SERVICE_PROVIDERS[sys.argv[1]]
-oauth_client = service_provider.get_oauth_client()
-
-resp, content = oauth_client.request(service_provider.test_url, 'GET')
-print 'Response: %s' % str(resp)
-print 'Content: %s' % str(content)
+for i in xrange(0, len(service_provider.access_tokens)):
+    oauth_client = service_provider.get_oauth_client(token_index=i)
+    resp, content = oauth_client.request(service_provider.test_url, 'GET')
+    print 'Token %d' % i
+    print '    Response: %s' % str(resp)
+    print '    Content: %s' % str(content)
