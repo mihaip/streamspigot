33f148cb77271f4574ded8c4bef8ef0d0df15b40
Mihai Parparita <mihai@persistent.info>
2013-11-28 10:34:08 -0800

    Block Feedfetcher requests.
    Sorry FRBE :(

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -10,13 +10,22 @@ class BlockingMiddleware(object):
         self._wrapped_app = app
 
     def __call__(self, environ, start_response):
-        user_agent = environ.get('HTTP_USER_AGENT', None)
+        user_agent = environ.get('HTTP_USER_AGENT', '')
         # Scraper running on EC2 (IP addresses 50.18.2.106 and 50.18.73.38)
         # that's requesting lots of Tweet Digest pages.
         if user_agent == 'Python-urllib/2.7':
             logging.info('Blocked request')
             start_response('403 Forbidden', [('Content-type','text/plain')])
             return ['']
+
+        # Google Reader and iGoogle are dead, yet their crawler still lives on.
+        # (and generates ~1,200 requests per day). Block it, since presumably no
+        # one is actually looking at the resuts.
+        if user_agent.startswith('Feedfetcher-Google;'):
+            logging.info('Blocked Feedfetcher request')
+            start_response('403 Forbidden', [('Content-type','text/plain')])
+            return ['']
+
         return self._wrapped_app(environ, start_response)
 
 def webapp_add_wsgi_middleware(app):
