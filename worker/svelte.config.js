fb2f85d84a5f0a488cfad66eb2e8ee3e45d2f3df
Mihai Parparita <mihai@persistent.info>
2025-07-20 20:42:07 -0700

    Migrate to @sveltejs/adapter-cloudflare
    As described by https://svelte.dev/docs/kit/adapter-cloudflare#Migrating-from-Workers-Sites

--- worker/svelte.config.js
+++ worker/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare-workers";
+import adapter from "@sveltejs/adapter-cloudflare";
 import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +7,7 @@ const config = {
     // for more information about preprocessors
     preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter(),
-    },
+    kit: {adapter: adapter()},
 };
 
 export default config;
